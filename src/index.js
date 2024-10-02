33935961a3fa6540bea8ea73855be66b753bf03c
Soumyadip Maity <22053029@kiit.ac.in>
2024-10-02 20:52:10 +0530

    Refactor database connection logic and project configuration files

--- src/index.js
+++ src/index.js
@@ -8,8 +8,17 @@ dotenv.config({
     path:'./env'
 });
 
-connectDB();
+connectDB()
 
+.then(()=>{
+    app.listen(process.env.PORT || 8000, () =>{
+        console.log(`server is running at port ${process.env.PORT}`);
+        
+    })
+})
+.catch((err) =>{
+    console.log("MONGO DB CONNECTION FAILED", err);
+})
 
 
 
